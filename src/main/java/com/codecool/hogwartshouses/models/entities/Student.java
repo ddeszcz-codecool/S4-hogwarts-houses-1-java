dccb8eb3f6c31fe1b357356038f7f243ff5363ff
Dawid Deszcz <d.deszcz@gmail.com>
2023-02-06 14:46:48 +0100

    Point 1. Locus Omnis! ||| Create Student Entity

--- src/main/java/com/codecool/hogwartshouses/models/entities/Student.java
+++ src/main/java/com/codecool/hogwartshouses/models/entities/Student.java
@@ -1,12 +1,61 @@
 package com.codecool.hogwartshouses.models.entities;
 
+import com.codecool.hogwartshouses.models.types.PetType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+
+@AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@Entity
+@Table(name = "students")
 public class Student {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "student_id", nullable = false)
+    private Long id;
+
+    private String name;
+    private PetType petType;
+    @ManyToOne
+    @JoinColumn(name = "room_room_id")
+    private Room room;
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
 }
+
