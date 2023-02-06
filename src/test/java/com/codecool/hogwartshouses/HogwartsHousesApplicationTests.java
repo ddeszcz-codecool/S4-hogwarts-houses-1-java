6afd416f9559d910e1a66a550552c026147b7d76
Dawid Deszcz <d.deszcz@gmail.com>
2023-02-06 14:19:05 +0100

    Point 1. Locus Omnis! ||| Update POM

--- src/test/java/com/codecool/hogwartshouses/HogwartsHousesApplicationTests.java
+++ src/test/java/com/codecool/hogwartshouses/HogwartsHousesApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HogwartsHousesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
