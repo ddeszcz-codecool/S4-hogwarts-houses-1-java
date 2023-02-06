472e4c3853228f9b29a7c8b6fb68e7d11e6de06e
Dawid Deszcz <d.deszcz@gmail.com>
2023-02-06 14:40:29 +0100

    Point 1. Locus Omnis! ||| Create Room Entity

--- src/main/java/com/codecool/hogwartshouses/models/entities/Room.java
+++ src/main/java/com/codecool/hogwartshouses/models/entities/Room.java
@@ -1,12 +1,30 @@
 package com.codecool.hogwartshouses.models.entities;
 
+import com.codecool.hogwartshouses.models.types.HouseType;
+import com.codecool.hogwartshouses.models.types.PetType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+
 @NoArgsConstructor
-@Builder
+@AllArgsConstructor
+@Entity
+@Table(name = "rooms")
 public class Room {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "room_id", nullable = false)
+    private Long id;
+
+
+    private HouseType houseType;
+    private Student student;
+    private PetType petType;
+
+
 
 }
