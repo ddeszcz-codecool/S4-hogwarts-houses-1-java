0c6c58e7ad23afefed86ec97ea28a617215dddfd
Dawid Deszcz <d.deszcz@gmail.com>
2023-02-08 15:26:04 +0100

    Point 1. Locus Omnis! ||| Update repository RoomDAO interface

--- src/main/java/com/codecool/hogwartshouses/service/DAO/RoomDAO.java
+++ src/main/java/com/codecool/hogwartshouses/service/DAO/RoomDAO.java
@@ -1,5 +1,12 @@
 package com.codecool.hogwartshouses.service.DAO;
 
+import com.codecool.hogwartshouses.model.Room;
+
+import java.util.Set;
+
 public interface RoomDAO {
+
+    Set<Room> getAllRooms();
+    void addRoom(Room room);
 
 }
