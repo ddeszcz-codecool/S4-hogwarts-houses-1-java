bb033ddb609a4051acde63760253b47fa222a355
Dawid Deszcz <d.deszcz@gmail.com>
2023-02-08 16:18:38 +0100

    Point 1. Locus Omnis! ||| Update RoomService class

--- src/main/java/com/codecool/hogwartshouses/service/RoomService.java
+++ src/main/java/com/codecool/hogwartshouses/service/RoomService.java
@@ -1,13 +1,22 @@
 package com.codecool.hogwartshouses.service;
 
+import com.codecool.hogwartshouses.model.Room;
 import com.codecool.hogwartshouses.service.DAO.RoomDAO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class RoomService {
 
-    @Autowired
-    private RoomDAO roomDAO;
 
+    private final RoomDAO roomDAO;
+
+    public RoomService(RoomDAO roomDAO) {
+        this.roomDAO = roomDAO;
+    }
+
+    public Set<Room> getAllRooms() {
+        return roomDAO.getAllRooms();
+    }
 }
