2624950c835b1f95e4c560a8ed57adf6b8e49ba2
Dawid Deszcz <d.deszcz@gmail.com>
2023-02-08 15:20:09 +0100

    Point 1. Locus Omnis! ||| Update Room entity

--- src/main/java/com/codecool/hogwartshouses/model/Room.java
+++ src/main/java/com/codecool/hogwartshouses/model/Room.java
@@ -1,12 +1,24 @@
 package com.codecool.hogwartshouses.model;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.codecool.hogwartshouses.model.types.HouseType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.*;
+import org.springframework.data.annotation.Id;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @Builder
+@AllArgsConstructor
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private HouseType houseType;
+    private int capacity;
+
 
 }
