fa350ad6cdc2987114a01c387ce06b1aede07149
Dawid Deszcz <d.deszcz@gmail.com>
2023-02-08 16:50:15 +0100

    Point 2. Locus Novus! ||| Add room capacity condition

--- src/main/java/com/codecool/hogwartshouses/controller/RoomController.java
+++ src/main/java/com/codecool/hogwartshouses/controller/RoomController.java
@@ -32,7 +32,10 @@ public class RoomController {
 
 
     @PostMapping
-    public String addRoom(@RequestBody Room room){
+    public String addRoom(@RequestBody Room room) {
+        if (room.getCapacity() < 2) {
+            throw new IllegalArgumentException("Room must have minimum capacity of two persons");
+        }
         roomService.addRoom(room);
         return "redirect:";
     }
